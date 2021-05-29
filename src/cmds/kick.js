695d39118f49d81d9bc218f9607b35280113b452
Skilouxe <74900067+OhSkilouxe@users.noreply.github.com>
2021-05-29 12:58:53 +0200

    Update kick.js

--- src/cmds/kick.js
+++ src/cmds/kick.js
@@ -10,7 +10,7 @@ exports.run = (client, message, args) => {
     let member = (message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0])));
     if(!member) return message.reply(':x: | Please enter a valid member.')
 
-    let reason = args.join(' ').slice(args[0].length + 1)
+    const reason = args.join(' ').slice(args[0].length + 1)
     if(!reason) return message.reply(':x: | Please supply a reason.')
 
     message.guild.member(member).kick(reason).then(() => {
